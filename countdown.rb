a264b205aceb671529785cddf799da0a267fd7b9
Marvin Reyes <reyemarv@gmail.com>
2020-02-01 15:42:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  
+  number = number
+  
+  while number > 0
+   puts "#{number}SECOND(S)!"
+   number -= 1 
+  end 
+  
+    "HAPPY NEW YEAR!"
 end
+
+  
+    
