ffbd8d3fde36b6f959fe08feaa18f3d1e14066f7
Marvin Reyes <reyemarv@gmail.com>
2020-02-01 16:20:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -16,5 +16,13 @@ def countdown_with_sleep(x)
   x = x 
     
     sleep(5)
+      while number > 0 
+        puts "#{number} SECONDS(S)"
+        number -= 1 
+      end 
+    
+      "HAPPY NEW YEAR"
+end 
+
   
     
